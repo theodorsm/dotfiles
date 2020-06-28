90d8506c70c623d9edd1ba7d2683881e46d7a6ec
theodorsm <theodor@midtlien.com>
2020-06-29 01:01:21 +0200

    Fix scrolling with mouse and add second monitor setup

--- bspwm/startup.sh
+++ bspwm/startup.sh
@@ -1,16 +1,30 @@
 #! /bin/bash
+
 setxkbmap eu &
 feh --bg-scale ~/Pictures/mountains_fog_sky_116917_3840x2160.jpg &
+
+# second monitor setup
+if xrandr --listactivemonitors | grep "eDP"; then
+    xrandr --output DP-1 --mode 2560x1440 --rate 99.95
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 320x1440 --rotate normal --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
+fi &
+
 ~/.config/polybar/launch.sh &
 compton &
 xidlehook --not-when-fullscreen --not-when-audio --timer 600 'systemctl suspend' '' &
-#fix java stuff
+
+# fix java stuff
 wmname LG3D &
-#keyrepeat
+
+# keyrepeat
 xset r rate 200 40 &
-#touchpad
+
+# touchpad
 xinput set-prop 11 "libinput Tapping Enabled" 1 &
-xinput set-prop 11 "libinput Natural Scrolling Enabled" 1 &
-#100hz second monitor
-xrandr --output DP-1 --mode 2560x1440 --rate 99.95 &
+if xinput --list | grep "SABRE"; then
+    xinput set-prop 11 "libinput Natural Scrolling Enabled" 0
+else
+    xinput set-prop 11 "libinput Natural Scrolling Enabled" 1
+fi &
+
 
